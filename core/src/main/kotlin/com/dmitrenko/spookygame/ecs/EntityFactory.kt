dc01755a04d7d043cf51618b0b3d0f2b261063dc
AahzBrut <partyomenko@gmail.com>
2020-08-25 18:07:12 +0300

    Исправил

--- core/src/main/kotlin/com/dmitrenko/spookygame/ecs/EntityFactory.kt
+++ core/src/main/kotlin/com/dmitrenko/spookygame/ecs/EntityFactory.kt
@@ -11,27 +11,25 @@ import ktx.ashley.entity
 import ktx.ashley.with
 
 class EntityFactory(private val game: SpookyGame) {
-	private val mobKnight = Texture(Gdx.files.internal("graphics/entity/unit/mob/knight/idle/Tuscan_idle_00000.png"))
+    private val mobKnight = Texture(Gdx.files.internal("graphics/entity/unit/mob/knight/idle/Tuscan_idle_00000.png"))
 
-	fun getEntity(type: EntityType): Entity {
-		when (type) {
-			PLAYER -> return getPlayer()
-			else -> throw Throwable("Entity does not exist")
-		}
-	}
+    fun getEntity(type: EntityType): Entity {
+        when (type) {
+            PLAYER -> return getPlayer()
+            else -> throw Throwable("Entity does not exist")
+        }
+    }
 
-	private fun getPlayer(): Entity {
-		return game.engine.entity {
-			with<TransformComponent> {
-				position.set(1f, 1f, 0f)
-			}
-			with<GraphicComponent>{
-				sprite.run {
-					setRegion(mobKnight)
-					setSize(PLAYER_SIZE_X, PLAYER_SIZE_Y)
-					setOriginCenter()
-				}
-			}
-		}
-	}
+    private fun getPlayer(): Entity {
+        return game.engine.entity {
+            with<TransformComponent> {
+                position.set(1f, 1f, 0f)
+                size.set(PLAYER_SIZE_X, PLAYER_SIZE_Y)
+            }
+            with<GraphicComponent> {
+                sprite.setRegion(mobKnight)
+                sprite.setOriginCenter()
+            }
+        }
+    }
 }
